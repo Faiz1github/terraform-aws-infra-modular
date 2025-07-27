a6138975baf992b60bc053ded4e14dfa5651ecad
Faiz1git <mdfaizanmohammed@gmail.com>
2025-07-27 16:58:02 +0530

    module resuable

--- main.tf
+++ main.tf
@@ -3,18 +3,18 @@ module "vpc" {
   vpc_cidr       = var.vpc_cidr
   public_subnets = var.public_subnets
   azs            = var.azs
-  name           = "demo"
+  name           = "demo-ap-south-1-testing"
 }
 
 module "sg" {
   source  = "./modules/sg"
-  name    = "demo"
+  name    = "demo-ap-south-1-testing"
   vpc_id  = module.vpc.vpc_id
 }
 
 module "ec2" {
   source    = "./modules/ec2"
-  name      = "demo"
+  name      = "demo-ap-south-1-testing"
   ami       = var.ami
   subnet_id = module.vpc.public_subnet_ids[0]
   sg_id     = module.sg.sg_id
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,6 +1,6 @@
 vpc_cidr       = "10.0.0.0/16"
 public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-azs            = ["us-east-1a", "us-east-1b"]
-ami            = "ami-034568121cfdea9c3"  # Amazon Linux 2
+azs            = ["apsouth-1a", "ap-south-1b"]
+ami            = "ami-008a9f4478d7401d4"  # Amazon Linux 2
 key_name       = "terraformodule"
 
